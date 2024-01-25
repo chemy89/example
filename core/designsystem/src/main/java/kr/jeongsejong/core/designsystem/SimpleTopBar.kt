1cd1d12eecaee88691775dc7b8c26e5ef5afe259
jeongsejong <hiro9789@naver.com>
2024-01-25 23:14:14 +0900

    font size 지정

--- core/designsystem/src/main/java/kr/jeongsejong/core/designsystem/SimpleTopBar.kt
+++ core/designsystem/src/main/java/kr/jeongsejong/core/designsystem/SimpleTopBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,7 +18,10 @@ fun SimpleTopBar(
 ) {
     CenterAlignedTopAppBar(
         title = {
-            Text(text = title)
+            Text(
+                text = title,
+                fontSize = 20.sp
+            )
         },
         navigationIcon = {
             IconButton(
