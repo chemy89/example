af80ed3310e7f9d1ae4cf2711c83fd783e024651
jeongsejong <hiro9789@naver.com>
2024-01-26 00:30:12 +0900

    불필요한 dependency 제거

--- app/src/main/java/kr/jeongsejong/example/ParkingViewModel.kt
+++ app/src/main/java/kr/jeongsejong/example/ParkingViewModel.kt
@@ -1,21 +1,8 @@
 package kr.jeongsejong.example
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.map
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import kr.jeongsejong.core.data.repository.AccountRepository
-import kr.jeongsejong.core.data.repository.ImageSearchRepository
-import kr.jeongsejong.core.data.vo.DocumentData
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
